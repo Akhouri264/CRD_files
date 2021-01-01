4cebcef21bbf55399ddfe73cdb47c18ed9489cb0
Akhouri264 <cu.17bcs1198@gmail.com>
2021-01-01 14:29:34 +0530

    CRD_project_file

---
+++ accessing.py
@@ -0,0 +1,55 @@
+#here are the commands to demonstrate how to access and perform operations on a main file
+#run the MODULE of MAIN FILE and import main file as a library
+import sys
+import code as x 
+from code import create
+#importing the main file("code" is the name of the file I have used) as a library 
+if sys.version_info < (3, 0):
+    from thread import *
+else:
+    from threading import Thread
+    
+x.create("srctest",25)
+#to create a key with key_name,value given and with no time-to-live property
+
+x.create("src",70,3600)
+#to create a key with key_name,value given and with time-to-live property value given(number of seconds)
+
+x.read("srctest")
+#it returns the value of the respective key in Json object format 'key_name:value'
+
+
+x.read("src")
+#it returns the value of the respective key in Json object format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
+
+
+x.create("srctest",50)
+#it returns an ERROR since the key_name already exists in the database
+#To overcome this error use delete operation and recreate it
+
+x.delete("srctest")
+#it deletes the respective key and its value from the database(memory is also freed)
+
+#we can access these using multiple threads like
+key_name = "hello"
+value = 5
+timeout=300
+
+t1=Thread(target=(x.create or x.read or x.delete),args=(key_name,value,timeout)) #as per the operation
+t1.start()
+
+
+key_name = "test"
+value = 100
+timeout=100
+
+t2=Thread(target=(x.create or x.read or x.delete),args=(key_name,value,timeout)) #as per the operation
+t2.start()
+
+#and so on upto tn
+
+#the code also refturns other errors like
+#"invalidkey" if key_length is greater than 32 or key_name contains any numeric,special characters etc.,
+#"key doesnot exist" if key_name was mis-spelt or deleted earlier
+#"File memory limit reached" if file memory exceeds 1GB
+
---
+++ code.py
@@ -0,0 +1,93 @@
+import sys
+if sys.version_info < (3, 0):
+    from thread import *
+else:
+    from threading import Thread
+import time
+data_dict = dict()
+def raise_exception(message):
+    """
+    Used to raise exception
+    :param message:                 (str)       --  Message and raises its exception
+    :return: None
+    """
+    raise Exception(message)
+
+def is_not_expired(time_to_compare, key):
+    """
+    Used to check if time has expired for that
+    :param time_to_compare:         (time)      --  end time value for that key
+    :param key:                     (str)       --  name of key to raise exception
+    :return: bool:
+                True: If time not expired
+    """
+    if time_to_compare != 0:
+        if time.time() < time_to_compare:
+            return True
+        else:
+            raise_exception("Error: Time-to-live of %s expired" % key)
+            return False
+    return True
+
+def create(key,value,timeout=0):
+    """
+    Used to create a key value file system for that key name
+    :param key:                     (str)       --  name of key
+    :param value:                   (any)       --  value of that file
+    :param timeout:                 (int)       --  time to live of this file
+    :return: None
+    """
+    if key in data_dict.keys():
+        raise_exception("Error: Key exists")
+    else:
+        if key.isalpha():
+            if len(data_dict) < (1024*1024*1024) and value <= (16*1024*1024):
+                if timeout == 0:
+                    value_list = [value, timeout]
+                else:
+                    value_list = [value, time.time() + timeout]
+                if len(key) <= 32: # key name constraints
+                    data_dict[key] = value_list
+            else:
+                raise_exception("Error: Memory limit exceeded!")
+        else:
+            raise_exception("Error: Invalid key_name!")
+
+def read(key):
+    """
+    Used to return value associated with the key
+    :param key:                     (str)       --  name of key
+    :return: value of that key in json format
+    """
+    if key not in data_dict.keys():
+        raise_exception("Error: Key Not present!")
+    else:
+        value_list = data_dict[key]
+        if is_not_expired(value_list[1], key):
+            ret_value = str(key) + ":" + str(value_list[0])
+            return ret_value
+
+def deleting_key(key):
+    """
+    Used to delete the key
+    :param key:                     (str)       --  name of key
+    :return: None
+    """
+    try:
+        del data_dict[key]
+        print("Key deleted successfully..")
+    except Exception as e:
+        raise_exception(str(e))
+
+def delete(key):
+    """
+    Used to delete the key
+    :param key:                     (str)       --  name of key
+    :return: None
+    """
+    if key not in data_dict.keys():
+        raise_exception("Error: Key Not present!")
+    else:
+        value_list = data_dict[key]
+        if is_not_expired(value_list[1], key):
+            deleting_key(key)
